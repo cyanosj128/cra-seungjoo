cd97069ada5e246369e6b7b686e52ebfe6a245aa
cyanosj128 <cyanosj128@gmail.com>
2023-12-29 19:29:18 +0900

    Enable envs for different phases

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,33 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
+module.exports = (env) => {
+  return {
+    entry: './src/index.tsx',
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      new Dotenv({
+        path: `./.env${env && env.production ? '.production' : ''}`, // Use .env.production for production
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+  };
 };
